0267fdb16937df78698aaf07c9d4b2b4b3748ff9
daksh chauhan <chauhandaksh60@gmail.com>
2025-02-20 00:44:49 +0530

    commit

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar.jsx'
-import Messagecontainer from '../../components/messagecontainer.jsx'
+import Messagecontainer from '../../components/messagecontainer/Messagecontainer.jsx'
 const Home = () => {
   return (
     <div className='flex sm:h-[455px] md:h-[550px] rounded-lg  overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter bg-opacity-0 backdrop-blur-lg'>
